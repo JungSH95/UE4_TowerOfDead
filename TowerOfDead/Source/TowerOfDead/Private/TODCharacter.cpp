f8c34ab5a131f83e8aaa1419cb5d96256796ff3e
JungSH <jungs6734@gmail.com>
2020-11-02 23:57:30 +0900

    SoulRecovery UI 작업 중간저장

--- TowerOfDead/Source/TowerOfDead/Private/TODCharacter.cpp
+++ TowerOfDead/Source/TowerOfDead/Private/TODCharacter.cpp
@@ -36,7 +36,7 @@ ATODCharacter::ATODCharacter()
 	WeaponTrigger->SetupAttachment(GetMesh(), FName(TEXT("Weapon")));
 	WeaponTrigger->SetGenerateOverlapEvents(false);
 
-	GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -88.0f), FRotator(0.0f, -90.0f, 0.0f));
+	GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -100.0f), FRotator(0.0f, -90.0f, 0.0f));
 	
 	GetCapsuleComponent()->SetCollisionProfileName(TEXT("TODCharacter"));
 
